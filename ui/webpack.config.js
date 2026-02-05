8c4cac330a2c88ff6f7292b32e98c85137ead046
Connor Yates <cyates@hopskipdrive.com>
2026-02-05 15:56:10 -0800

    refactor: migrate UI to TypeScript

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,38 +1,58 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
-extName = "GitDiffExtension"
-
-module.exports = {
-  entry: './src/index.js',
+const config = {
+  // 1. Entry: Now pointing to .tsx
+  entry: {
+    extension: "./src/index.tsx",
+  },
   output: {
-    filename: `extensions-${extName}.js`,
-    path: __dirname + `/dist/resources/extension-${extName}.js`,
-    libraryTarget: 'window',
-    library: ["tmp", "extensions"],
+    // 2. Output: Keep filename 'main.js' so your GitHub Action doesn't break
+    filename: "main.js",
+    path: __dirname + "/dist",
+    libraryTarget: "window",
+    library: ["extensionsAPI", "git-diff-extension"],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        terserOptions: {
+          format: { comments: false },
+        },
+        extractComments: false,
+      }),
+    ],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        // 3. Loader: Use esbuild-loader for .ts and .tsx files
+        test: /\.(ts|js)x?$/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "tsx",
+          target: "es2015",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  }
 };
+
+module.exports = config;
